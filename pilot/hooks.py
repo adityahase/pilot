fadc38e37b3089e691064fae0ad753b1674ae480
Aditya Hase <aditya@adityahase.com>
2024-07-18 16:21:00 +0530

    chore: Export python type annotations

--- pilot/hooks.py
+++ pilot/hooks.py
@@ -221,9 +221,8 @@ app_license = "agpl-3.0"
 # ]
 
 # Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
+export_python_type_annotations = True
 
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-
--- pilot/provision/doctype/region/region.py
+++ pilot/provision/doctype/region/region.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class Region(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		access_token: DF.Password | None
+		cloud_provider: DF.Link
+		status: DF.Literal["Draft", "Pending", "Active", "Archived"]
+		title: DF.Data
+		vpc_cidr_block: DF.Data | None
+		vpc_id: DF.Data | None
+	# end: auto-generated types
+
 	pass
