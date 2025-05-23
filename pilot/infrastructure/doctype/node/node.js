4133ee923eea411e7b1d508aeffa4b5f0fdcb031
Aditya Hase <aditya@adityahase.com>
2025-05-23 13:17:50 +0530

    feat(infra): Create ping playbook
    - Uses ansible.builtin.ping
    - Works as a sanity test for ansible integration

--- pilot/infrastructure/doctype/node/node.js
+++ pilot/infrastructure/doctype/node/node.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2025, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Node", {
-// 	refresh(frm) {
+frappe.ui.form.on("Node", {
+	refresh(frm) {
+		const ping_actions = [[__("Ping"), "ping"]];
 
-// 	},
-// });
+		for (const [label, method] of ping_actions) {
+			// eslint-disable-next-line no-inner-declarations
+			async function callback() {
+				const res = await frm.call(method);
+				if (res.message) {
+					frappe.msgprint(res.message);
+				} else {
+					frm.refresh();
+				}
+			}
+			frm.add_custom_button(label, callback, __("Actions"));
+		}
+	},
+});
--- pilot/infrastructure/doctype/node/node.py
+++ pilot/infrastructure/doctype/node/node.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+from pilot.infrastructure.ansible import Ansible
 
 
 class Node(Document):
-	pass
+	@frappe.whitelist()
+	def ping(self):
+		Ansible(
+			playbook="ping.yml",
+			node=self,
+			user="ubuntu",
+			port="22",
+		).run()
