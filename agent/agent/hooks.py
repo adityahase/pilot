cc3ef814242ac3a20e140647d01efe7b17d0b762
Aditya Hase <aditya@adityahase.com>
2025-06-02 23:16:44 +0530

    chore(agent): Move setup code to Machine

--- agent/agent/hooks.py
+++ agent/agent/hooks.py
@@ -1,4 +1,4 @@
-from agent.machine import Machine, download_artifacts
+from agent.machine import Machine
 from agent.network import FirecrackerBridge
 
 EGRESS_INTERFACE = "eth0"
@@ -8,7 +8,21 @@ async def startup():
 	bridge = FirecrackerBridge(egress_interface=EGRESS_INTERFACE)
 	await bridge.setup()
 
-	await download_artifacts()
+	artifacts = [
+		(
+			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/vmlinux-6.1.128",
+			"vmlinux-6.1.128",
+		),
+		(
+			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/ubuntu-24.04.squashfs",
+			"ubuntu-24.04.squashfs",
+		),
+	]
+	await Machine.download_artifacts(artifacts)
+	rootfs = [
+		"ubuntu-24.04.squashfs",
+	]
+	await Machine.prepare_rootfs(rootfs)
 
 	machine = Machine("1")
 	await machine.setup()
--- agent/agent/machine.py
+++ agent/agent/machine.py
@@ -47,25 +47,7 @@ class Machine:
 		await self.run(f"cp {ARTIFACTS_ROOT}/vmlinux-6.1.128 {self.kernel_file}")
 
 	async def setup_rootfs(self):
-		await self.run(f"unsquashfs -f -d {self.squashfs_root} {ARTIFACTS_ROOT}/ubuntu-24.04.squashfs")
-		await self.setup_ssh_key()
-		await self.setup_dns()
-		await self.make_ext4_filesystem()
-
-	async def setup_ssh_key(self):
-		await self.run('ssh-keygen -t ed25519 -f id_ed25519 -N ""')
-		await self.run(f"mv id_ed25519.pub {self.squashfs_root}/root/.ssh/authorized_keys")
-		await self.run(f"mv id_ed25519 {self.private_key_file}")
-
-	async def setup_dns(self):
-		resolv_conf_path = os.path.join(CHROOT_PATH, self.squashfs_root.lstrip("/"), "etc/resolv.conf")
-		with open(resolv_conf_path, "w") as f:
-			f.write("nameserver 8.8.8.8")
-
-	async def make_ext4_filesystem(self):
-		await self.run(f"chown -R root:root {self.squashfs_root}")
-		await self.run(f"truncate -s 400M {self.rootfs_file}")
-		await self.run(f"mkfs.ext4 -d {self.squashfs_root} -F {self.rootfs_file}")
+		await self.run(f"cp {ARTIFACTS_ROOT}/ubuntu-24.04.ext4 {self.rootfs_file}")
 		await self.run(f"chown {self.uid}:{self.gid} {self.rootfs_file}")
 		await self.run(f"chmod 600 {self.rootfs_file}")
 
@@ -87,6 +69,7 @@ class Machine:
 		)
 		await self.run(command)
 
+	@classmethod
 	async def run(self, cmd):
 		args = shlex.split(cmd)
 		args = ["chroot", CHROOT_PATH, *args]
@@ -101,32 +84,65 @@ class Machine:
 			raise SubprocessError
 		return process
 
+	@classmethod
+	async def download_artifacts(cls, files):
+		os.makedirs(os.path.join(CHROOT_PATH, ARTIFACTS_ROOT.lstrip("/")), exist_ok=True)
+		for url, filename in files:
+			file = os.path.join(CHROOT_PATH, ARTIFACTS_ROOT.lstrip("/"), filename)
+			# Write the file to the host filesystem
+			if not os.path.exists(file):
+				await cls.download_file(url, file)
 
-async def download_file(url, filename):
-	with requests.get(url, stream=True) as r:
-		r.raise_for_status()
-		with open(filename, "wb") as f:
-			for chunk in r.iter_content(chunk_size=8192):
-				f.write(chunk)
+	@classmethod
+	async def download_file(cls, url, filename):
+		with requests.get(url, stream=True) as r:
+			r.raise_for_status()
+			with open(filename, "wb") as f:
+				for chunk in r.iter_content(chunk_size=8192):
+					f.write(chunk)
 
+	@classmethod
+	async def prepare_rootfs(cls, files):
+		os.makedirs(os.path.join(CHROOT_PATH, ARTIFACTS_ROOT.lstrip("/")), exist_ok=True)
 
-async def download_artifacts():
-	os.makedirs(os.path.join(CHROOT_PATH, ARTIFACTS_ROOT.lstrip("/")), exist_ok=True)
-	files = [
-		(
-			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/vmlinux-6.1.128",
-			"vmlinux-6.1.128",
-		),
-		(
-			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/ubuntu-24.04.squashfs",
-			"ubuntu-24.04.squashfs",
-		),
-	]
-	for url, filename in files:
-		file = os.path.join(CHROOT_PATH, ARTIFACTS_ROOT.lstrip("/"), filename)
-		# Write the file to the host filesystem
-		if not os.path.exists(file):
-			await download_file(url, file)
+		async def unsquash_rootfs(squashfs_file, squashfs_root):
+			await cls.run(f"unsquashfs -f -d {squashfs_root} {squashfs_file}")
+
+		async def setup_ssh_key(ssh_key_file, squashfs_root):
+			await cls.run(f'ssh-keygen -t ed25519 -f {ssh_key_file} -N ""')
+			await cls.run(f"cp {ssh_key_file}.pub {squashfs_root}/root/.ssh/authorized_keys")
+
+		async def setup_dns(squashfs_root):
+			resolv_conf_path = os.path.join(CHROOT_PATH, squashfs_root.lstrip("/"), "etc/resolv.conf")
+			with open(resolv_conf_path, "w") as f:
+				f.write("nameserver 8.8.8.8")
+
+		async def make_ext4_filesystem(squashfs_root, rootfs_file):
+			await cls.run(f"chown -R root:root {squashfs_root}")
+			await cls.run(f"truncate -s 400M {rootfs_file}")
+			await cls.run(f"mkfs.ext4 -d {squashfs_root} -F {rootfs_file}")
+
+		async def cleanup_temporary_files():
+			await cls.run(f"rm -rf {squashfs_root}")
+
+		for file in files:
+			name = file.rsplit(".", 1)[0]
+			squashfs_file = os.path.join(ARTIFACTS_ROOT, file)
+			squashfs_root = os.path.join(ARTIFACTS_ROOT, f"{name}-squashfs")
+			rootfs_file = os.path.join(ARTIFACTS_ROOT, f"{name}.ext4")
+			ssh_key_file = os.path.join(ARTIFACTS_ROOT, f"{name}-id_ed25519")
+
+			if os.path.exists(os.path.join(CHROOT_PATH, rootfs_file.lstrip("/"))):
+				continue
+
+			await unsquash_rootfs(squashfs_file, squashfs_root)
+
+			if os.path.exists(os.path.join(CHROOT_PATH, ssh_key_file.lstrip("/"))):
+				await setup_ssh_key(ssh_key_file, squashfs_root)
+
+			await setup_dns(squashfs_root)
+			await make_ext4_filesystem(squashfs_root, rootfs_file)
+			await cleanup_temporary_files()
 
 
 JSON = """{
