6d5b4ca792c8a436e23289ac725cd3ca62d034c7
Aditya Hase <aditya@adityahase.com>
2025-06-05 20:27:14 +0530

    chore(agent): Use standardized names for firecracker artifacts

--- agent/agent/hooks.py
+++ agent/agent/hooks.py
@@ -9,21 +9,25 @@ async def startup():
 	bridge = FirecrackerBridge(egress_interface=EGRESS_INTERFACE)
 	await bridge.setup()
 
-	artifacts = [
+	firecracker_artifacts = [
+		(
+			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/vmlinux-5.10.233",
+			"ubuntu-24.04-firecracker-linux-5.x.kernel",
+		),
 		(
 			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/vmlinux-6.1.128",
-			"vmlinux-6.1.128",
+			"ubuntu-24.04-firecracker.kernel",
 		),
 		(
 			"https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/ubuntu-24.04.squashfs",
-			"ubuntu-24.04.squashfs",
+			"ubuntu-24.04-firecracker.squashfs",
 		),
 	]
-	await Firecracker.download_artifacts(artifacts)
-	rootfs = [
-		"ubuntu-24.04.squashfs",
+	await Firecracker.download_artifacts(firecracker_artifacts)
+	firecracker_rootfs = [
+		"ubuntu-24.04-firecracker.squashfs",
 	]
-	await Firecracker.prepare_rootfs(rootfs)
+	await Firecracker.prepare_rootfs(firecracker_rootfs)
 
 	releases = [
 		"focal",
