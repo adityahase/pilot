65d186ef08eb08986bfdf447bc6da4678aa77ba4
Aditya Hase <aditya@adityahase.com>
2025-06-05 20:39:43 +0530

    chore(infra): Bootstrap list of development images

--- pilot/bootstrap/bootstrap.py
+++ pilot/bootstrap/bootstrap.py
@@ -27,10 +27,51 @@ NODES = [
 	},
 ]
 
+IMAGES = [
+	{
+		"name": "Ubuntu 20.04",
+		"kernel": "focal.kernel",
+		"initial_ram_disk": "focal.initrd",
+		"root_filesystem": "focal.rootfs",
+	},
+	{
+		"name": "Ubuntu 22.04",
+		"kernel": "jammy.kernel",
+		"initial_ram_disk": "jammy.initrd",
+		"root_filesystem": "jammy.rootfs",
+	},
+	{
+		"name": "Ubuntu 24.04",
+		"kernel": "noble.kernel",
+		"initial_ram_disk": "noble.initrd",
+		"root_filesystem": "noble.rootfs",
+	},
+	{
+		"name": "Ubuntu Firecracker 24.04",
+		"kernel": "ubuntu-24.04-firecracker.kernel",
+		"root_filesystem": "ubuntu-24.04-firecracker.rootfs",
+	},
+	{
+		"name": "Ubuntu Firecracker 24.04 Linux 5.x",
+		"kernel": "ubuntu-24.04-firecracker-linux-5.x.kernel",
+		"root_filesystem": "ubuntu-24.04-firecracker.rootfs",
+	},
+]
+
 
 def setup():
 	for node in NODES:
+		if frappe.db.exists("Node", node["name"]):
+			continue
 		document = frappe.new_doc("Node")
 		for key, value in node.items():
+			document.set(key, value)
+		document.insert()
+
+	for image in IMAGES:
+		if frappe.db.exists("Image", image["name"]):
+			continue
+		document = frappe.new_doc("Image")
+		for key, value in image.items():
 			document.set(key, value)
 		document.insert()
